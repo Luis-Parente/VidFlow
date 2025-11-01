e49f5656e67b66a1b228fdc19da2de275def0158
Luis Parente <lgsp25@hotmail.com>
2025-11-01 08:38:44 -0300

    feita transição do projeto para o vite

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
+import axios from "axios";
+
 const containerVideos = document.querySelector(".videos__container");
 
 async function buscarEMostrarVideos() {
   try {
-    const busca = await fetch("http://localhost:3000/videos");
-    const videos = await busca.json();
+    const { data: videos } = await axios.get("http://localhost:3000/videos");
 
     videos.forEach((video) => {
       if (video.categoria == "") {
